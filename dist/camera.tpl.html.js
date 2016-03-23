d9288d046e28dd436726e2c3daaf0dd2c315d2b0
Brian Leathem <bleathem@gmail.com>
2016-03-23 12:34:00 -0700

    Initial styling of the camera

--- dist/camera.tpl.html.js
+++ dist/camera.tpl.html.js
@@ -12,14 +12,20 @@ ngModule.run(['$templateCache', function ($templateCache) {
     ' Copyright 2016 Red Hat, Inc. and/or its affiliates.\n' +
     ' This is unpublished proprietary source code of Red Hat.\n' +
     '-->\n' +
-    '<div layout="column">\n' +
+    '<div class="wfm-camera" flex>\n' +
+    '  <md-button class="md-icon-button" aria-label="Back" ng-click="ctrl.cancel()" flex>\n' +
+    '    <md-icon md-font-set="material-icons">arrow_back</md-icon>\n' +
+    '  </md-button>\n' +
+    '  <video  ng-show="ctrl.cameraOn" autoplay style="transform: rotateY(180deg)"></video>\n' +
+    '  <canvas ng-hide="ctrl.cameraOn"></canvas>\n' +
     '  <md-button class="md-raised" ng-hide="ctrl.cameraOn" ng-click="ctrl.startCamera()">Start Camera</md-button>\n' +
-    '  <md-button class="md-raised" ng-show="ctrl.cameraOn" ng-click="ctrl.stopCamera()">Stop Camera</md-button>\n' +
-    '  <md-card>\n' +
-    '    <video  ng-show="ctrl.cameraOn" autoplay style="transform: rotateY(180deg);"></video>\n' +
-    '    <canvas ng-hide="ctrl.cameraOn"></canvas>\n' +
-    '  </md-card>\n' +
-    '  <md-button class="md-raised" ng-show="ctrl.cameraOn" ng-click="ctrl.snap()">Snap Photo</md-button>\n' +
+    '  <div class="wfm-camera-actions">\n' +
+    '    <md-button  class="wfm-camera-btn" ng-click="ctrl.snap()"></md-button>\n' +
+    '  </div>\n' +
+    '</div>\n' +
+    '\n' +
+    '<div layout="column">\n' +
+    '\n' +
     '</div>\n' +
     '');
 }]);
