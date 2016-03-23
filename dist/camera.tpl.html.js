598f5de46a0c39993fa20bb20982ec124619bfbd
Brian Leathem <bleathem@gmail.com>
2016-03-23 16:51:16 -0700

    Adopted Andres' stlying of the camera component

--- dist/camera.tpl.html.js
+++ dist/camera.tpl.html.js
@@ -13,19 +13,20 @@ ngModule.run(['$templateCache', function ($templateCache) {
     ' This is unpublished proprietary source code of Red Hat.\n' +
     '-->\n' +
     '<div class="wfm-camera" flex>\n' +
-    '  <md-button class="md-icon-button wfm-camera-close" aria-label="Back" ng-click="ctrl.cancel()" flex>\n' +
-    '    <md-icon md-font-set="material-icons">close</md-icon>\n' +
+    '  <md-button class="md-icon-button" aria-label="Back" ng-click="ctrl.cancel()" flex>\n' +
+    '    <md-icon md-font-set="material-icons">arrow_back</md-icon>\n' +
     '  </md-button>\n' +
-    '  <video  ng-show="ctrl.cameraOn" autoplay style="transform: rotateY(180deg)"></video>\n' +
+    '  <video  ng-show="ctrl.cameraOn" autoplay></video>\n' +
     '  <canvas ng-hide="ctrl.cameraOn"></canvas>\n' +
-    '  <md-button class="md-raised" ng-hide="ctrl.cameraOn" ng-click="ctrl.startCamera()">Start Camera</md-button>\n' +
-    '  <div class="wfm-camera-actions">\n' +
-    '    <md-button  class="wfm-camera-btn" ng-click="ctrl.snap()"></md-button>\n' +
+    '  <div class="wfm-camera-actions" style="z-index: 1000">\n' +
+    '    <md-button ng-show="ctrl.cameraOn" class="wfm-camera-btn" ng-click="ctrl.snap()"></md-button>\n' +
+    '    <md-button ng-hide="ctrl.cameraOn" class="wfm-camera-confirmation-btn md-warn" ng-click="ctrl.startCamera()">\n' +
+    '      <md-icon md-font-set="material-icons">close</md-icon>\n' +
+    '    </md-button>\n' +
+    '    <md-button ng-hide="ctrl.cameraOn" class="wfm-camera-confirmation-btn" ng-click="ctrl.done()">\n' +
+    '      <md-icon md-font-set="material-icons">check</md-icon>\n' +
+    '    </md-button>\n' +
     '  </div>\n' +
-    '</div>\n' +
-    '\n' +
-    '<div layout="column">\n' +
-    '\n' +
     '</div>\n' +
     '');
 }]);
